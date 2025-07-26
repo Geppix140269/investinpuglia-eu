1864ef3b3f4d85a1b43efab0fdd2899b87852f5c
Giuseppe Funaro <139779768+Geppix140269@users.noreply.github.com>
2025-07-26 18:00:05 +0200

    Update sanity.ts

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client'
 
 export const sanity = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_DATASET,
   useCdn: true,
   apiVersion: '2023-07-09',
 })
