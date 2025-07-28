c78f131f73ebc623f3959a480bbafde544b512cb
Geppix140269 <139779768+Geppix140269@users.noreply.github.com>
2025-07-28 08:36:35 +0200

    test: Add locations to navbar - testing mobile view

--- sanity-studio/schemaTypes/index.ts
+++ sanity-studio/schemaTypes/index.ts
@@ -4,12 +4,16 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import { locationPageType } from './locationPage'
+import { seoType } from './objects/seo'
 
 export const schemaTypes = [
-  post, 
-  author, 
-  category, 
+  post,
+  author,
+  category,
   blockContent,
   calculatorConfigType,
-  calculationResultType
+  calculationResultType,
+  locationPageType,
+  seoType
 ]
