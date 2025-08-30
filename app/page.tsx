1b2df12190ff3cbbfa5fa109cc595a8c93713f41
Geppix140269 <139779768+Geppix140269@users.noreply.github.com>
2025-08-30 15:11:41 +0200

    Expand sitemap from 8 to 50+ pages for better SEO coverage

--- app/page.tsx
+++ app/page.tsx
@@ -27,22 +27,25 @@ export default function HomePage() {
         <ExitIntentPopup />
       </div>
 
-      {/* STREAMLINED MOBILE-FIRST HOMEPAGE FOR CONVERSION */}
+      {/* Main Homepage Sections */}
       <HeroVisual />
-      <SignAgreementBanner />
+      
+      {/* IMPORTANT: Grant Institutions with EU and Regione Puglia logos */}
+      <GrantInstitutions />
+      
       <OpportunitySection />
       <HowItWorks />
       <AboutGiuseppe />
+      <Services />
+      <WhyPuglia />
+      <TrulloSection />
+      <ApulinkShowcase />
+      <SuccessStories />
       <FAQ />
       <CTASection />
-
-      {/* MOVED TO SEPARATE PAGES FOR BETTER SEO & LESS CLUTTER */}
-      {/* <GrantInstitutions /> - Move to /about or /how-it-works */}
-      {/* <WhyPuglia /> - Move to /about or dedicated landing page */}
-      {/* <Services /> - Already has /services page */}
-      {/* <ApulinkShowcase /> - Move to /tools or /services */}
-      {/* <TrulloSection /> - Move to /tools or separate page */}
-      {/* <SuccessStories /> - Move to /about or testimonials page */}
+      
+      {/* Sign Agreement Banner moved to bottom */}
+      <SignAgreementBanner />
     </>
   )
 }
